11a9c155a19cc38bc0d6a7b4124c01e3d165f518
Kinan Bab <Kinan.bab.14@gmail.com>
2017-07-09 22:10:08 -0300

    Clean up

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,7 +1,5 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+// Maps one url to an array of corresponding urls. 
+// The content of all/majority of these urls should match that of the original url.
 var urls = {
     "http://34.211.84.202:8080/jquery-2.2.4.min.js": [
         "http://34.211.84.202:8080/jquery-2.2.4.min.js", 
@@ -15,112 +13,122 @@ var urls = {
  * @param {function(string)} callback - called when the URL of the current tab is found.
  */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
+  var queryInfo = { // Get the active tab in the active window.
     active: true,
     currentWindow: true
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
+  chrome.tabs.query(queryInfo, function(tabs) { // tabs: array of tabs that match the query.
+    callback(tabs[0].url); // Exactly one tab is active in any window.
   });
 }
 
-function getAllUrls(urls, callback, errorCallback) {
-  function getOneUrl(results, index) {
-    if(index >= urls.length) {
-      callback(results);
-      return;
-    }
-    
-    console.log("Start: " + urls[index]);
-    
-    var x = new XMLHttpRequest();
-    x.open("GET", urls[index], true);
-    x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    x.setRequestHeader('Access-Control-Allow-Origin', '*');
-    x.onload = function() {
-      var response = x.responseText;
-      results.push(response);
-      
-      getOneUrl(results, index+1);
-    };
-    x.onerror = function(error) {
-      console.log("ERROR: " + urls[index]);
-      console.log(error);
-      errorCallback("Network error.");
-    };
-    
-    x.send();
+/**
+ * Get the content of the given urls.
+ * @param {array} urls - an array of urls.
+ * @return {array} - an array of promises, each of which is a promise to the content of 
+ *                   the url that matches its index.
+ */
+function getContents(urls) {
+  var promises = [];
+  for(var i = 0; i < urls.length; i++) {
+    promises.push(new Promise(function(resolve, reject) {
+      var x = new XMLHttpRequest();
+      x.open("GET", urls[i], true);
+      x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      x.setRequestHeader('Access-Control-Allow-Origin', '*');
+      x.onload = function() { resolve(x.responseText); };
+      x.onerror = function(error) { reject(error); };
+      x.send();
+    }));
   }
   
-  getOneUrl([], 0);
+  return promises;
 }
 
+/**
+ * Compute the frequency of every string in results.
+ * @param {array} urls - an array of source urls.
+ * @param {array} results - an array containing the content for every given url (matches by index).
+ * @return {object} - an object where the key is a source code string and the value is its frequency and source urls.
+ */
+function computeFrequencies(urls, results) {
+  var frequencies = {};
+  for(var i = 0; i < results.length; i++) {
+    if(frequencies[results[i]] == null) {
+      var obj = { "frequency": 1, "urls": [ urls[i] ] };
+      frequencies[results[i]] = obj;
+    }
+    
+    else {
+      var obj = frequencies[results[i]];
+      obj.frequency++;
+      obj.urls.push(urls[i]);
+    }
+  }
+      
+  return frequencies;
+}
+
+/**
+ * Construct the HTML table for displaying the results.
+ * @param {object} frequencies - the key is a source code string and the value is its frequency and source urls.
+ * @return {string} - the HTML for the results table.
+ */
+function constructHTML(frequencies) {
+  var html = "<tr><th>Source URLs</th><th>Votes</th></tr>";
+  for(var key in frequencies) {
+    if(!frequencies.hasOwnProperty(key)) continue;
+    
+    var obj = frequencies[key];
+    html += "<tr><td>"+obj.urls.join("<br>")+"</td>";
+    html += "<td>"+obj.frequency+"</td></tr>";
+  }
+  return html;
+}
+
+/**
+ * Show the status message and empty the result table.
+ * @param {string} statusText - the text to be displayed in the HTML.
+ */
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+  document.getElementById("status").textContent = statusText;
+  document.getElementById("result-table").innerHTML = "";
 }
 
+/*
+ * When the extension is loaded do the following:
+ * 1. Get the currently open url.
+ * 2. If the url is supported/registered in the extension:
+ *    a. Get the contents of the corresponding urls.
+ *    b. Compute the frequency of every version of the source code.
+ *    c. Check for consensus and display the frequencies in a table.
+ */
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
+    // Check if the current url is supported.
     if(urls[url] == null) {
       renderStatus("Not Applicable");
       return;
     }
     
+    // Get the content of all urls corresponding to the currently open url.
     renderStatus('Checking for consensus');
-    getAllUrls(urls[url], function(results) {
-      var votes = {};
+    var promises = getContents(urls[url]);
+    
+    Promise.all(promises).then(function(results) {
+      // Compute the frequencies and display the result.
+      var frequencies = computeFrequencies(urls[url], results);
+      var consensusReached = frequencies[results[0]].frequency == results.length;
+      var htmlResultTable = constructHTML(frequencies);
       
-      for(var i = 0; i < results.length; i++) {
-        if(votes[results[i]] == null) {
-          var obj = { "votes": 1, "urls": [ urls[url][i] ] };
-          votes[results[i]] = obj;
-        }
-        
-        else { 
-          var obj = votes[results[i]];
-          obj.votes++;
-          obj.urls.push(urls[url][i]);
-        }
-      }
-      
-      var count = 0;
-      var tableBody = "<tr><th>Source URL</th><th>Votes</th></tr>";
-      for(var key in votes) {
-        if(!votes.hasOwnProperty(key)) continue;
-        
-        var obj = votes[key];
-        tableBody += "<tr><td>"+obj.urls[0]+"</td><td>"+obj.votes+"</td></tr>";
-        count++;
-      }
-      
-      if(count == 1) {        
+      if(consensusReached) {    
         renderStatus('Consensus Reached!');
-        document.getElementById("result-table").innerHTML = tableBody;
+        document.getElementById("result-table").innerHTML = htmlResultTable;
       } else {
         renderStatus('Results ready');
-        document.getElementById("result-table").innerHTML = tableBody;
+        document.getElementById("result-table").innerHTML = htmlResultTable;
       }
-    }, renderStatus);
+    });//.catch(renderStatus);
   });
 });
